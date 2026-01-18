c3c39c54d94f7f8a8fe4f4920eaacd6f95a83c48
Shantanu Kamble <shantanusudhirkamble.india@gmail.com>
2026-01-18 19:09:15 +0530

    first commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -104,7 +104,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/Shantanu Kamble Resume_2026-2025.pdf"
           download
         >
           Download CV{" "}
@@ -113,7 +113,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://linkedin.com/shan"
           target="_blank"
         >
           <BsLinkedin />
